f2e616ee8c2a74a8affb132ec55148e73da8ba0c
ophir <pere.jobs@gmail.com>
2019-11-29 00:10:45 +0100

    Simplification de la fonction main

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,17 @@
 use std::env;
 use std::process;
-use hangman::game::Game;
+
 use hangman::admin::Admin;
 use hangman::config;
+use hangman::game::Game;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let mode = config::parse_args(args).unwrap_or_else(|e| {
-        eprintln!("{}", e);
-        process::exit(1) });
+    let mode = config::parse_args(args)?;
     
     match mode {
-        config::Mode::Admin => run_admin(),
-        config::Mode::Game => run_game()
-    }
-}
-
-fn run_game() {
-    let game = Game::new();
-    match game {
-        Err(e) => { eprintln!("Error: {}", e); process::exit(1); },
-        _ => match game.unwrap().run() {
-            Err(e) => { eprintln!("Error: {}", e); process::exit(1); },
-            _ => process::exit(0)
-        }
-    }
-}
-
-fn run_admin() {
-    let admin = Admin::new();
-    match admin {
-        Err(e) => { eprintln!("Error: {}", e); process::exit(1); },
-        _ => match admin.unwrap().run() {
-            Err(e) => { eprintln!("Error: {}", e); process::exit(1); },
-            _ => process::exit(0)
-        }
+        config::Mode::Admin => Admin::new()?.run(),
+        config::Mode::Game => Game::new()?.run()
     }
 }
